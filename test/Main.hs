5018d9c3ae6ccebefbe04d6c345970b1ecb0c3a0
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-12-15 12:19:54 +1100

    vogued

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE
-    OverloadedStrings
-  #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -8,19 +6,19 @@ import           Control.Applicative
 import           Control.Monad.Trans
 import           Data.Aeson
 import           Data.Bits
-import           Data.ByteString(ByteString)
-import qualified Data.ByteString.Lazy.Char8 as BSL
-import           Data.HashMap.Strict(HashMap)
-import qualified Data.HashMap.Strict as H
+import           Data.ByteString                    (ByteString)
+import qualified Data.ByteString.Lazy.Char8         as BSL
+import           Data.HashMap.Strict                (HashMap)
+import qualified Data.HashMap.Strict                as H
 import           Data.Maybe
 import           Data.Monoid
-import           Data.Text(Text)
+import           Data.Text                          (Text)
 import           Data.Word
 import           Test.Hspec
 import           Test.HUnit.Base
 
-import           Vaultaire.Types
 import           Vaultaire.Collector.Common.Process
+import           Vaultaire.Types
 
 import           Ceilometer.Process
 import           Ceilometer.Types
