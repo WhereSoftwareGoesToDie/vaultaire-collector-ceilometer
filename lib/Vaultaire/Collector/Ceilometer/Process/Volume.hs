25d38b7f37b916956340b59c7636a5de229380a1
Oswyn Brent <oswyn.brent@anchor.com.au>
2015-02-11 16:06:37 +1100

    Reupdate volume using cleaned up common

--- lib/Vaultaire/Collector/Ceilometer/Process/Volume.hs
+++ lib/Vaultaire/Collector/Ceilometer/Process/Volume.hs
@@ -21,37 +21,46 @@ import           Vaultaire.Collector.Ceilometer.Types
 processVolumeEvent :: Metric -> Collector [(Address, SourceDict, TimeStamp, Word64)]
 processVolumeEvent = processEvent getVolumePayload
 
-parseVolumeStatus :: Text -> Maybe Word8
-parseVolumeStatus x = review pfVolumeStatus <$> parseVolumeStatus' x
-  where
-    parseVolumeStatus' "error"     = Just VolumeError
-    parseVolumeStatus' "available" = Just VolumeAvailable
-    parseVolumeStatus' "creating"  = Just VolumeCreating
-    parseVolumeStatus' "extending" = Just VolumeExtending
-    parseVolumeStatus' "deleting"  = Just VolumeDeleting
-    parseVolumeStatus' "attaching" = Just VolumeAttaching
-    parseVolumeStatus' "detaching" = Just VolumeDetaching
-    parseVolumeStatus' "in-use"    = Just VolumeInUse
-    parseVolumeStatus' "retyping"  = Just VolumeRetyping
-    parseVolumeStatus' "uploading" = Just VolumeUploading
-    parseVolumeStatus' _           = Nothing
+parseVolumeStatus :: Text -> Maybe PFVolumeStatus
+parseVolumeStatus "error"     = Just VolumeError
+parseVolumeStatus "available" = Just VolumeAvailable
+parseVolumeStatus "creating"  = Just VolumeCreating
+parseVolumeStatus "extending" = Just VolumeExtending
+parseVolumeStatus "deleting"  = Just VolumeDeleting
+parseVolumeStatus "attaching" = Just VolumeAttaching
+parseVolumeStatus "detaching" = Just VolumeDetaching
+parseVolumeStatus "in-use"    = Just VolumeInUse
+parseVolumeStatus "retyping"  = Just VolumeRetyping
+parseVolumeStatus "uploading" = Just VolumeUploading
+parseVolumeStatus _           = Nothing
 
-parseVolumeVerb :: Text -> Maybe Word8
-parseVolumeVerb x = review pfVolumeVerb <$> parseVolumeVerb' x
-  where
-    parseVolumeVerb' "create" = Just VolumeCreate
-    parseVolumeVerb' "resize" = Just VolumeResize
-    parseVolumeVerb' "delete" = Just VolumeDelete
-    parseVolumeVerb' "attach" = Just VolumeAttach
-    parseVolumeVerb' "detach" = Just VolumeDetach
-    parseVolumeVerb' "update" = Just VolumeUpdate
-    parseVolumeVerb' _        = Nothing
+parseVolumeVerb :: Text -> Maybe PFVolumeVerb
+parseVolumeVerb "create" = Just VolumeCreate
+parseVolumeVerb "resize" = Just VolumeResize
+parseVolumeVerb "delete" = Just VolumeDelete
+parseVolumeVerb "attach" = Just VolumeAttach
+parseVolumeVerb "detach" = Just VolumeDetach
+parseVolumeVerb "update" = Just VolumeUpdate
+parseVolumeVerb _        = Nothing
 
 -- | Constructs the compound payload for volume events
 getVolumePayload :: Metric -> IO (Maybe Word64)
 getVolumePayload m@Metric{..} = do
     components <- case T.splitOn "." <$> getEventType m of
-        Just (_:verb:endpoint:__) -> return $ Just (verb, endpoint)
+        Just (_:verb:endpoint:__) -> do
+            v <- case parseVolumeVerb verb of
+                Just x  -> return $ Just x
+                Nothing -> do
+                    alertM "Ceilometer.Process.getVolumePayload"
+                         $ "Invalid verb for volume event: " <> show verb
+                    return Nothing
+            e <- case parseEndpoint (Just endpoint) of
+                Just x  -> return $ Just x
+                Nothing -> do
+                    alertM "Ceilometer.Process.getVolumePayload"
+                         $ "Invalid endpoint for volume event: " <> show endpoint
+                    return Nothing
+            return $ liftA2 (,) v e
         Just x -> do
             alertM "Ceilometer.Process.getVolumePayload"
                  $ "Invalid parse of verb + endpoint for volume event" <> show x
@@ -60,7 +69,7 @@ getVolumePayload m@Metric{..} = do
             alertM "Ceilometer.Process.getVolumePayload"
                    "event_type field missing from volume event"
             return Nothing
-    statusValue <- case H.lookup "status" metricMetadata of
+    status <- case H.lookup "status" metricMetadata of
         Just (String status) -> case parseVolumeStatus status of
             Just x  -> return $ Just x
             Nothing -> do
@@ -75,26 +84,8 @@ getVolumePayload m@Metric{..} = do
             alertM "Ceilometer.Process.getVolumePayload"
                    "Status field missing from volume event"
             return Nothing
-    case components of
-        Just (verb, endpoint) -> do
-            verbValue <- case parseVolumeVerb verb of
-                Just x  -> return $ Just x
-                Nothing -> do
-                    alertM "Ceilometer.Process.getVolumePayload" $
-                           "Invalid verb for volume event: " <> show verb
-                    return Nothing
-            endpointValue <- case parseEndpoint (Just endpoint) of
-                Just x -> return $ Just x
-                Nothing -> do
-                    alertM "Ceilometer.Process.getVolumePayload" $
-                           "Invalid endpoint for volume event: " <> show endpoint
-                    return Nothing
-            let payload = case metricPayload of
-                    Just p -> do
-                        sv <- statusValue
-                        vv <- verbValue
-                        ev <- endpointValue
-                        return $ constructCompoundPayload sv vv ev (fromIntegral p)
-                    Nothing -> Nothing
-            return payload
-        Nothing -> return Nothing
+    return $ do
+        (v, e) <- components
+        s      <- status
+        p      <- fromIntegral <$> metricPayload
+        return $ review (prCompoundEvent . pdVolume) $ PDVolume s v e p
